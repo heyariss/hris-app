996855b71719c62c5191d2bbca9943f4d9a2538a
ARIS <68618848+riskiarissandy@users.noreply.github.com>
2022-11-29 16:34:20 +0700

    first commit

---
+++ app/Http/Controllers/API/CompanyController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class CompanyController extends Controller
+{
+    public function fetch(Request $request)
+    {
+        $id = $request->input('id');
+        $name = $request->input('name');
+        $limit = $request->input('limit', 10); //maksimal mengambil datanya
+
+        $companyQuery = Company::with(['users'])->whereHas('users', function ($query) {
+            $query->where('user_id', Auth::id());
+        });
+
+        // Get single data
+        if ($id) {
+            $company = $companyQuery->find($id);
+
+            if ($company) {
+                return ResponseFormatter::success($company, 'Company found');
+            }
+
+            return ResponseFormatter::error('Company not found', 404);
+        }
+
+  // Get multiple data
+  $companies = company::whereHas('users', function ($query) {
+            $query->where('user_id', Auth::id());
+        });
+        
+        // Filtering
+        if($name) {
+            $companies ->where('name', 'like', '%'. $name . '%' );
+        }
+        
+        // Company::with(['user])->where('name', 'like', '%'. $name . '%kunde%')->paginate(10);
+        return ResponseFormatter::success(
+            $companies->paginate($limit),
+            'Companies Found'
+        );
+
+        
+    }
+
+    public function create(CreateCompanyRequest $request)
+    {
+        try {
+            // Upload logo
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+
+            // Create company
+            $company = Company::create([
+                'name' => $request->name,
+                'logo' => $path
+            ]);
+
+            if (!$company) {
+                throw new Exception('Company not created');
+            }
+
+            // Attach company to user
+            $user = User::find(Auth::id());
+            $user->companies()->attach($company->id);
+
+            // Load users at company
+            $company->load('users');
+
+            return ResponseFormatter::success($company, 'Company created');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+
+    public function update(UpdateCompanyRequest $request, $id)
+    {
+        try {
+            $company = Company::find($id);
+
+            if (!$company) {
+                throw new Exception('Company not found');
+            }
+
+            // Upload logo
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+
+            // Update company
+            $company->update([
+                'name' => $request->name,
+                'logo' => $path
+            ]);
+
+            // Load users at company
+            $company->load('users');
+
+            return ResponseFormatter::success($company, 'Company updated');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+}
---
+++ app/Models/Team.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Team extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function employe(){
+        $this->belongsTo(Employee::class, "team_id");
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'icon',
+        'company_id',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
