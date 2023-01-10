b008177cce63dc8f71d7a19d41d8f5066aae7663
ARIS <68618848+riskiarissandy@users.noreply.github.com>
2023-01-10 08:56:04 +0700

    update commit

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -25,7 +25,7 @@ class EmployeeController extends Controller
         $company_id = $request->input('company_id');
         $limit = $request->input('limit', 10);
 
-        $employeeQuery = Employee::query();
+        $employeeQuery = Employee::with('team', 'role');
 
         // Get single data
         if ($id) {
